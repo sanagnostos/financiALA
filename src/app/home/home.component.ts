a989b2e702189b2943e66476bc12fb00e31adbc7
Steven Anagnostos <stevenanagnostos@gmail.com>
2019-07-28 19:58:12 -0400

    fixed assets

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  //encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
   //images = [1, 2, 3].map(() => `~/src/assets/images/audi.jpg`);
