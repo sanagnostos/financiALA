d44dcef79bf8bd74baf594e8f97c53146f5744ef
Alex <adaguirre@email.neit.edu>
2019-08-06 17:25:32 -0400

    alex

--- src/app/finance/finance.component.ts
+++ src/app/finance/finance.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./finance.component.css']
 })
 export class FinanceComponent implements OnInit {
+  formatLabel(value: number | null) {
+    if (!value) {
+      return 0;
+    }
 
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
   constructor() { }
 
   ngOnInit() {
