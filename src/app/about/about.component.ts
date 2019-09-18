324506efbcd4d3d538afd185b4f5bae35d00af48
Alex <adaguirre@email.neit.edu>
2019-09-18 02:22:57 -0400

    About Manager

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { HttpClient } from '@angular/common/http';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  users: UserService[];
+  currentUser: any
 
-  constructor() { }
+  public show:boolean = false;
 
+  constructor(private us: UserService, private router: Router, private http: HttpClient, private loginservice: LoginService) { 
+    this.loginservice.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  get isManager() {
+    return this.currentUser && this.currentUser.rank == 3;
+}
+
+
+toggle() {
+  this.show = !this.show;
+}
   ngOnInit() {
+    this.us.getUser().subscribe((data: UserService[])=> {
+      this.users = data;
+      console.log(this.users)
+    })
+
   }
 
 }
